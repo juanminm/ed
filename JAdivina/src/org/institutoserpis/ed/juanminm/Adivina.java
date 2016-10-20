eaf7d20fab6652f9a8fdded1ef7175eddfac5ae8
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-10-20 14:02:42 +0200

    Finish JAdivina

--- JAdivina/src/org/institutoserpis/ed/juanminm/Adivina.java
+++ JAdivina/src/org/institutoserpis/ed/juanminm/Adivina.java
@@ -1,10 +1,13 @@
 package org.institutoserpis.ed.juanminm;
 
+import java.util.Random;
+
 public class Adivina {
 	public static void main(String[] args) {
+		Random random = new Random();
 		int numeroAleatorio;
 
-		numeroAleatorio = (int) (Math.random()*1000+1);
+		numeroAleatorio = random.nextInt(1000)+1;
 		System.out.println(numeroAleatorio);
 	}
 }
