4ab443ce6f49c647e37d48c5d9a627d852fd837a
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-10-20 14:01:21 +0200

    Add JAdivina

---
+++ JAdivina/src/org/institutoserpis/ed/juanminm/Adivina.java
@@ -0,0 +1,10 @@
+package org.institutoserpis.ed.juanminm;
+
+public class Adivina {
+	public static void main(String[] args) {
+		int numeroAleatorio;
+
+		numeroAleatorio = (int) (Math.random()*1000+1);
+		System.out.println(numeroAleatorio);
+	}
+}
