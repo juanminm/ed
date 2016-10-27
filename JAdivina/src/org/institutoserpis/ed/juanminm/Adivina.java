a63bcf4a73ffa6b9ffb40118d3a680ab0e2e76b6
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-10-27 13:26:10 +0200

    Finish program

--- JAdivina/src/org/institutoserpis/ed/juanminm/Adivina.java
+++ JAdivina/src/org/institutoserpis/ed/juanminm/Adivina.java
@@ -1,13 +1,30 @@
 package org.institutoserpis.ed.juanminm;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Adivina {
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		Random random = new Random();
-		int numeroAleatorio;
+		int numero, numeroAleatorio;
 
 		numeroAleatorio = random.nextInt(1000)+1;
 		System.out.println(numeroAleatorio);
+
+		do {
+			System.out.print("Introduce el número: ");
+			String line = scan.nextLine();
+			numero = Integer.parseInt(line);
+
+			if (numero <= numeroAleatorio) {
+				System.out.println("El numero introducido es menor.");
+			} else if (numero >= numeroAleatorio) {
+				System.out.println("El número introducido es mayor.");
+			} else {
+				System.out.println("¡Adivinaste el número!");
+			}
+
+		} while (numero != numeroAleatorio);
 	}
 }
